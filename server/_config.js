c6693257d34c86b978e47352278cffdce1d2f39c
topleft <pete.james.jeffryes@gmail.com>
2015-10-18 22:38:37 -0600

    set up mongolab with _config and .env files

--- server/_config.js
+++ server/_config.js
@@ -2,7 +2,8 @@ var config = {};
 
 config.mongoURI = {
 	test: 'mongodb://localhost/crud-test-mocha',
-	development: 'mongodb://localhost/crud-testing' 
+	development: 'mongodb://localhost/crud-testing',
+	production: proccess.env.MONGOLAB_URI 
 };
 
 module.exports = config;
