99d3d7cdffe4d18738eb6bf3fe733714f959b5d4
elvece <lvcifferello@gmail.com>
2015-11-18 12:13:02 -0700

    updated user schema and added hash before saving to db

--- server/database.js
+++ server/database.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
+var config = require('./_config');
 
 var itemSchema = new Schema ({
 	name: String,
@@ -8,14 +10,52 @@ var itemSchema = new Schema ({
 });
 
 var userSchema = new Schema({
-    username: String,
-    password: String
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    select: false
+  },
+  githubProfileID: {
+    type: String
+  },
 });
+
+// hash before saving to database
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash if the password is new or modified
+  if (!user.isModified('password')) return next();
+
+  // generate salt
+  bcrypt.genSalt(config.SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with the salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // overwrite the plain-text password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// verify for plain-text and hashed passwords
+userSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
 
 
 var Item = mongoose.model("items", itemSchema);
 var User = mongoose.model("users", userSchema);
-
 
 
 module.exports = {
