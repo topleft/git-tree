add06addb12bae747382c01ad0a679ff2103b0eb
topleft <pete.james.jeffryes@gmail.com>
2015-11-18 16:48:28 -0700

    git hub api endpoints

--- test/git-tests.js
+++ test/git-tests.js
@@ -3,34 +3,55 @@ process.env.NODE_ENV = 'test';
 var chai = require("chai");
 var chaiHttp = require('chai-http');
 var server = require("../server/app");
+var githubAPI = 'https://api.github.com'; 
 var should = chai.should();
 chai.use(chaiHttp);
 
-describe('git API routes', function(){
+describe('git API routes', function () {
 
-  describe('git repos', function(){
+  describe('git branches', function () {
 
-    xit('should return git repo as a json object', function(done){
-      chai.request(server)
-        .get('UPDATE ME')
+    it('should return git branch for user', function(done){
+      chai.request(githubAPI)
+        .get('/repos/topleft/git-tree/branches/master')
         .end(function(err, res){
+          console.log(res.body);
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('object');
+          res.body.commit.should.have.property('sha');
           done();
+        });
+
+      });
+  });
+
+  describe('git repos', function() {
+
+    xit('should return git repo as a json object', function(done) {
+      chai.request('https://')
+        .get('/repos/topleft/git-tree/branches/master')
+        .end(function(err, res){
+          console.log(res.body);
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.have.property('commit.sha');
+          res.body.should.be.a('object');
+          done();
+      });    
     });
 
-    xit('should return git repo with javascript as dominant language', function(){
+    xit('should return git repo with javascript as dominant language', function() {
 
     });
 
-    xit('should return git repo with python as dominant language', function(){
+    xit('should return git repo with python as dominant language', function() {
 
     });
 
   });
 
-  describe('git repos', function(){
+  describe('git repos', function() {
 
     xit('should return a list of git repos', function(){
       
@@ -46,3 +67,4 @@ describe('git API routes', function(){
 
   });
 });
+
