6485aacddbc3f28ad27d1c8ffd1aefe39cbdc0bb
elvece <lvcifferello@gmail.com>
2015-11-30 19:41:34 -0700

    added favicon and bower and notes to gitignore, nothing in tree directive

--- client/public/tree/tree-directive.js
+++ client/public/tree/tree-directive.js
@@ -7,7 +7,7 @@ angular.module('directives').directive('treeTemplate', ['repoFactory', 'alertFac
         },
         templateUrl: 'tree/tree.html',
         controller: function($scope){
-          
+
           $scope.getRepo = function(){
             repoFactory.getRepo($scope.repo.owner, $scope.repo.name)
               .success(function(data){
@@ -15,7 +15,7 @@ angular.module('directives').directive('treeTemplate', ['repoFactory', 'alertFac
                 repoFactory.getRepoDetails($scope.repo.owner, $scope.repo.name)
                   .success(function (data) {
                     console.log('Repo details"'+$scope.repo.name+'": ', data);
-                  })
+                  });
                 // make data appear on the screen
                 // make success message appear on the screen
               });
@@ -23,7 +23,7 @@ angular.module('directives').directive('treeTemplate', ['repoFactory', 'alertFac
             // if(){
 
             // }
-            
+
             // else{
             //   $scope.collections.newItem = {};
             //   alertFactory.add("danger", "Error! Item aready in database.");
