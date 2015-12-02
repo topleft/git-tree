0f473a598bb7690fff3491c125684ebddbbd77c0
elvece <lvcifferello@gmail.com>
2015-12-01 17:15:49 -0700

    removed form file from, from old project

--- client/public/auth/auth-controllers.js
+++ client/public/auth/auth-controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp');
 app.controller('authController', ['$scope', '$auth', '$rootScope','$window', '$location','authFactory', function($scope, $auth, $rootScope, $window, $location, authFactory){
 
 //setting user
-	$scope.authenticate = function(provider) {
+  $scope.authenticate = function(provider) {
     authFactory.authenticateUser(provider);
   };
 }]);
